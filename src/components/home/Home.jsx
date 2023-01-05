d30ef00fdfeb2fbd8919c8984538642da9cf400d
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-01-05 00:28:01 -0300

    dashboard and home components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,17 @@
+import Dashboard from "../dashboard/Dashboard";
 import Navbar from "../navbar/Navbar";
 import Sidebar from "../sidebar/Sidebar";
+// import { styled } from '@mui/material/styles';
+// import MuiAppBar from '@mui/material/AppBar';
+// import MuiDrawer from '@mui/material/Drawer';
 
 const Home = () => {
+
     return (
         <section>
             <Navbar/>
             <Sidebar/>
-            <h1>Home</h1>
+            <Dashboard/>
         </section>
     )
 };
