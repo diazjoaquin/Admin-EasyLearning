e34405a60d792775f00d4a90af6beebea7db7161
SantiRosso <santiagorosso@live.com.ar>
2023-01-09 21:12:56 -0300

    componente ChartBar con usuarios registrados por mes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
 import s from "./Statistics.module.css"
+import ChartBar from "./ChartBar";
 
 const Statistics = () => {
 
@@ -74,19 +75,22 @@ const Statistics = () => {
                 <h2 className={s.info2}>{state?.data?.videos}</h2>
                 
             </div>
-            <div className={s.infoContainer2}>
+                <div className={s.infoContainer2}>
+                    <div style={{width:"33%", height:"40vh"}}>
+                    <h2 className={s.title}>Only students/Teachers</h2>
+                    <Pie data={data} options={options}/>
+                </div>
                 <div style={{width:"33%", height:"40vh"}}>
-                <h2 className={s.title}>Only students/Teachers</h2>
-                <Pie data={data} options={options}/>
+                    <h2 className={s.title}>Chart</h2>
+                    <Pie data={data2} options={options}/>
+                </div>
+                <div style={{width:"33%", height:"40vh"}}>
+                    <h2 className={s.title}>Chart</h2>
+                    <Pie data={data3} options={options}/>
+                </div>
             </div>
-            <div style={{width:"33%", height:"40vh"}}>
-                <h2 className={s.title}>Chart</h2>
-                <Pie data={data2} options={options}/>
-            </div>
-            <div style={{width:"33%", height:"40vh"}}>
-                <h2 className={s.title}>Chart</h2>
-                <Pie data={data3} options={options}/>
-            </div>
+            <div className={s.infoContainer3}>
+                <ChartBar/>
             </div>
             
         </div>
