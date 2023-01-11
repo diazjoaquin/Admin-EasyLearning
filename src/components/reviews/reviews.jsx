b02748ff540c33693eda5e7443ebe70d24935e55
Fran3003 <franco.rantuchoQgmail.com>
2023-01-10 21:41:37 -0300

    refactor

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -6,11 +6,20 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Reviews = ()=> {
   const [value, setValue] = React.useState('1');
 
-  const [update, setUpdate] = useState(false)
+  // const [update, setUpdate] = useState(false)
   
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -22,16 +31,16 @@ const Reviews = ()=> {
     const result = await axios.get("http://localhost:3001/getAllReviewTotal")
     setState(result.data)
  }
-
+console.log(state);
 
  useEffect(() => {
     if(!state)
     getAllReviews()
- }, [state, setState, update])
+ }, [state, setState])
 
  const handleDeleted = async (id, string) => {
   console.log(id, string);
-  let response = await axios.delete(`http://localhost:3001/deleteComments/${id}/${string}`)
+  await axios.delete(`http://localhost:3001/deleteComments/${id}/${string}`)
   window.location.reload()
   
 }
@@ -39,6 +48,7 @@ const Reviews = ()=> {
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
       <TabContext value={value}>
+      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Reviews Page:" value="1" />
@@ -49,30 +59,100 @@ const Reviews = ()=> {
         <TabPanel value="1"> 
         {state?state[0].map(comment => (
           <h2 key={comment.id}>
-            <button onClick={() => handleDeleted(comment.id, "ReviewsPage")}>X</button>
-            <h4>{comment.title}</h4>
-            <h4>{comment.comments}</h4>
-          </h2>
+            <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon onClick={() => handleDeleted(comment.id, "ReviewsPage")}/>
+                    </IconButton>
+            <Divider variant="inset" component="li" />
+           <ListItem alignItems="flex-start">
+           <ListItemAvatar>
+             <Avatar src={comment.user.avatar} />
+           </ListItemAvatar>
+           <ListItemText
+             primary={comment.title}
+             secondary={
+               <React.Fragment>
+                 <Typography
+                   sx={{ display: 'inline' }}
+                   component="span"
+                   variant="body2"
+                   color="text.primary"
+                   >
+                   {comment.user.fullName} 
+                 </Typography>
+                 {" — "}{comment.comments}
+               </React.Fragment>
+             }
+             />
+         </ListItem>
+             </h2>
         )):undefined}</TabPanel>
+        
+        
         <TabPanel value="2">
           
         {state?state[1].map(comment => (
           <h2 key={comment.id}>
-            <button onClick={() => handleDeleted(comment.id, "ReviewsCourses")}>X</button>
-            <h4>{comment.title}</h4>
-            <h4>{comment.comments}</h4>
+            <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon onClick={() => handleDeleted(comment.id, "ReviewsCourses")}/>
+                    </IconButton>
+            <Divider variant="inset" component="li" />
+            <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+        <Avatar src={comment.user.avatar} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={comment.title}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {comment.user.fullName} 
+              </Typography>
+              {" — "}{comment.comments}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
           </h2>
         )):undefined}</TabPanel>
         <TabPanel value="3"><h2></h2>
         {state?state[2].map(comment => (
           <h2 key={comment.id}>
-            <button onClick={() => handleDeleted(comment.id, "CommentsVideos")}>X</button>
-            <h4>{comment.title}</h4>
-            <h4>{comment.description}</h4>
+            <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon onClick={() => handleDeleted(comment.id, "CommentsVideos")}/>
+                    </IconButton>
+            <Divider variant="inset" component="li" />
+            <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+        <Avatar alt="" src="" />
+        </ListItemAvatar>
+        <ListItemText
+          primary={comment.title}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {comment.userName} 
+              </Typography>
+              {" — "}{comment.description}
+            </React.Fragment>
+          }
+        />
+        </ListItem>
           </h2>
         )):undefined}</TabPanel>
+        </List>
       </TabContext>
     </Box>
   );
 }
 export default Reviews;
+
